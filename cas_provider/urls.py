44f03190970bb9dc8e3e715e378fb0c7794de6b7
Luka Čehovin <luka.cehovin@gmail.com>
2012-07-13 21:31:16 +0200

    Url reverse names

--- cas_provider/urls.py
+++ cas_provider/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
-    url(r'^login/', login),
-    url(r'^validate/', validate),
-    url(r'^logout/', logout),
+    url(r'^login/', login, name="cas_provider_login"),
+    url(r'^validate/', validate, name="cas_provider_validate"),
+    url(r'^logout/', logout, name="cas_provider_logout"),
 )
